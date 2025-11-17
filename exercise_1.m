7b1217b315b24e6cd96e676600ff573bbf3830c3
Pelopidas Tsiountsiouras <pelopidt@gmail.com>
2025-11-17 13:27:04 +0200

    All exercises of part 1

---
+++ exercise_1.m
@@ -0,0 +1,149 @@
+clc;
+close all;
+clear;
+
+%Functions to optimize
+f1 = @(x) 5^x + (2 - cos(x))^2;
+f2 = @(x) (x-1)^2 + exp(x-5) * sin(x+3);
+f3 = @(x) exp(-3*x) - (sin(x-2) - 2)^2;
+
+%Define the interval [a, b]
+a = -1;
+b = 3;
+
+%% Run the dichotomous method for each ε
+e_values = linspace (0.001,0.0049,100);
+l = 0.01;
+
+% Προκαταχώρηση πίνακα μετρήσεων
+evals_f1 = zeros(size(e_values));
+evals_f2 = zeros(size(e_values));
+evals_f3 = zeros(size(e_values));
+
+% --- Βρόχος για κάθε ε ---
+for k = 1:length(e_values)
+    e = e_values(k);
+
+    % Τρέχεις τον αλγόριθμο σου και επιστρέφεις:
+    
+    [~, ~, ~, evals_f1(k)] = dichotomousMethod(f1, a, b, l, e);
+    [~, ~, ~, evals_f2(k)] = dichotomousMethod(f2, a, b, l, e);
+    [~, ~, ~, evals_f3(k)] = dichotomousMethod(f3, a, b, l, e);
+end
+
+% --- Γραφικές Παραστάσεις ---
+figure;
+plot(e_values, evals_f1, '-o');
+xlabel('ε'); ylabel('Αριθμός αξιολογήσεων f_1(x)');
+title('Μεταβολή αξιολογήσεων για f_1');
+grid on;
+
+figure;
+plot(e_values, evals_f2, '-o');
+xlabel('ε'); ylabel('Αριθμός αξιολογήσεων f_2(x)');
+title('Μεταβολή αξιολογήσεων για f_2');
+grid on;
+
+figure;
+plot(e_values, evals_f3, '-o');
+xlabel('ε'); ylabel('Αριθμός αξιολογήσεων f_3(x)');
+title('Μεταβολή αξιολογήσεων για f_3');
+grid on;
+
+%% Run dichotomous method for each l
+e = 0.001;
+l_values = [1, 0.5, 0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.003];
+
+evals_f1 = zeros(size(l_values));
+evals_f2 = zeros(size(l_values));
+evals_f3 = zeros(size(l_values));
+
+for k = 1:length(l_values)
+    l = l_values(k);
+
+    % Τρέχεις τον αλγόριθμο σου και επιστρέφεις:
+    
+    [~, ~, ~, evals_f1(k)] = dichotomousMethod(f1, a, b, l, e);
+    [~, ~, ~, evals_f2(k)] = dichotomousMethod(f2, a, b, l, e);
+    [~, ~, ~, evals_f3(k)] = dichotomousMethod(f3, a, b, l, e);
+end
+
+% --- Γραφικές Παραστάσεις ---
+figure;
+plot(l_values, evals_f1, '-o');
+xlabel('l'); ylabel('Αριθμός αξιολογήσεων f_1(x)');
+title('Μεταβολή αξιολογήσεων για f_1');
+grid on;
+
+figure;
+plot(l_values, evals_f2, '-o');
+xlabel('l'); ylabel('Αριθμός αξιολογήσεων f_2(x)');
+title('Μεταβολή αξιολογήσεων για f_2');
+grid on;
+
+figure;
+plot(l_values, evals_f3, '-o');
+xlabel('l'); ylabel('Αριθμός αξιολογήσεων f_3(x)');
+title('Μεταβολή αξιολογήσεων για f_3');
+grid on;
+
+%% Plot the interval bounds for different interval lengths
+e = 0.001;
+l_values = [1, 0.5, 0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.003];
+
+figure;
+hold on; grid on;
+for i = 1:length(l_values)
+    l = l_values(i);
+
+    [k, a_list, b_list, ~] = dichotomousMethod(f1, a, b, l, e);
+
+    it = 1:length(a_list);  % δείκτης επαναλήψεων
+
+    plot(it, a_list, '-o', 'DisplayName', sprintf('a_k, l=%.4f', l));
+    plot(it, b_list, '--o', 'DisplayName', sprintf('b_k, l=%.4f', l));
+end
+
+xlabel('k');
+ylabel('Τιμές a_k και b_k');
+title('Εξέλιξη άκρων [a_k, b_k] για f_1(x)');
+legend show;
+hold off;
+
+figure;
+hold on; grid on;
+for i = 1:length(l_values)
+    l = l_values(i);
+
+    [k, a_list, b_list, ~] = dichotomousMethod(f2, a, b, l, e);
+
+    it = 1:length(a_list);  % δείκτης επαναλήψεων
+
+    plot(it, a_list, '-o', 'DisplayName', sprintf('a_k, l=%.4f', l));
+    plot(it, b_list, '--o', 'DisplayName', sprintf('b_k, l=%.4f', l));
+end
+
+xlabel('k');
+ylabel('Τιμές a_k και b_k');
+title('Εξέλιξη άκρων [a_k, b_k] για f_2(x)');
+legend show;
+hold off;
+
+figure;
+hold on; grid on;
+for i = 1:length(l_values)
+    l = l_values(i);
+
+    [k, a_list, b_list, ~] = dichotomousMethod(f3, a, b, l, e);
+
+    it = 1:length(a_list);  % δείκτης επαναλήψεων
+
+    plot(it, a_list, '-o', 'DisplayName', sprintf('a_k, l=%.4f', l));
+    plot(it, b_list, '--o', 'DisplayName', sprintf('b_k, l=%.4f', l));
+end
+
+xlabel('k');
+ylabel('Τιμές a_k και b_k');
+title('Εξέλιξη άκρων [a_k, b_k] για f_3(x)');
+legend show;
+hold off;
---
+++ exercise_4.m
@@ -0,0 +1,114 @@
+clc;
+close all;
+clear;
+
+%Functions to optimize
+syms x;
+f1 = 5^x + (2 - cos(x))^2;
+f2 = (x-1)^2 + exp(x-5) * sin(x+3);
+f3 = exp(-3*x) - (sin(x-2) - 2)^2;
+
+%Derivatives of f1, f2, f3
+diff_f1 = diff(f1, x);
+diff_f2 = diff(f2, x);
+diff_f3 = diff(f3, x);
+
+%Define the interval [a, b]
+a = -1;
+b = 3;
+
+%% Run dichotomous using derivatives method for each l
+l_values = [1, 0.5, 0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.003];
+
+evals_f1 = zeros(size(l_values));
+evals_f2 = zeros(size(l_values));
+evals_f3 = zeros(size(l_values));
+
+for k = 1:length(l_values)
+    l = l_values(k);
+
+    % Τρέχεις τον αλγόριθμο σου και επιστρέφεις:
+    
+    [~, ~, ~, evals_f1(k)] = dichotomousDerivativeMethod(diff_f1, a, b, l);
+    [~, ~, ~, evals_f2(k)] = dichotomousDerivativeMethod(diff_f2, a, b, l);
+    [~, ~, ~, evals_f3(k)] = dichotomousDerivativeMethod(diff_f3, a, b, l);
+end
+
+% --- Γραφικές Παραστάσεις ---
+figure;
+plot(l_values, evals_f1, '-o');
+xlabel('l'); ylabel('Αριθμός αξιολογήσεων f_1(x)');
+title('Μεταβολή αξιολογήσεων για f_1');
+grid on;
+
+figure;
+plot(l_values, evals_f2, '-o');
+xlabel('l'); ylabel('Αριθμός αξιολογήσεων f_2(x)');
+title('Μεταβολή αξιολογήσεων για f_2');
+grid on;
+
+figure;
+plot(l_values, evals_f3, '-o');
+xlabel('l'); ylabel('Αριθμός αξιολογήσεων f_3(x)');
+title('Μεταβολή αξιολογήσεων για f_3');
+grid on;
+
+%% Plot the interval bounds for different interval lengths
+l_values = [1, 0.5, 0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.003];
+
+figure;
+hold on; grid on;
+for i = 1:length(l_values)
+    l = l_values(i);
+
+    [k, a_list, b_list, ~] = dichotomousDerivativeMethod(diff_f1, a, b, l);
+
+    it = 1:length(a_list);  % δείκτης επαναλήψεων
+
+    plot(it, a_list, '-o', 'DisplayName', sprintf('a_k, l=%.4f', l));
+    plot(it, b_list, '--o', 'DisplayName', sprintf('b_k, l=%.4f', l));
+end
+
+xlabel('k');
+ylabel('Τιμές a_k και b_k');
+title('Εξέλιξη άκρων [a_k, b_k] για f_1(x)');
+legend show;
+hold off;
+
+figure;
+hold on; grid on;
+for i = 1:length(l_values)
+    l = l_values(i);
+
+    [k, a_list, b_list, ~] = dichotomousDerivativeMethod(diff_f2, a, b, l);
+
+    it = 1:length(a_list);  % δείκτης επαναλήψεων
+
+    plot(it, a_list, '-o', 'DisplayName', sprintf('a_k, l=%.4f', l));
+    plot(it, b_list, '--o', 'DisplayName', sprintf('b_k, l=%.4f', l));
+end
+
+xlabel('k');
+ylabel('Τιμές a_k και b_k');
+title('Εξέλιξη άκρων [a_k, b_k] για f_2(x)');
+legend show;
+hold off;
+
+figure;
+hold on; grid on;
+for i = 1:length(l_values)
+    l = l_values(i);
+
+    [k, a_list, b_list, ~] = dichotomousDerivativeMethod(diff_f3, a, b, l);
+
+    it = 1:length(a_list);  % δείκτης επαναλήψεων
+
+    plot(it, a_list, '-o', 'DisplayName', sprintf('a_k, l=%.4f', l));
+    plot(it, b_list, '--o', 'DisplayName', sprintf('b_k, l=%.4f', l));
+end
+
+xlabel('k');
+ylabel('Τιμές a_k και b_k');
+title('Εξέλιξη άκρων [a_k, b_k] για f_3(x)');
+legend show;
+hold off;
